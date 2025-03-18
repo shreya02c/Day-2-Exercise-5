661629717c4fe3796233fc529cf633975595551a
shreya02c <shreyachaudhari996@gmail.com>
2025-03-18 13:05:46 +0530

    Add files via upload

---
+++ Add.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div id="app">
+      <h1>CSS is great!</h1>
+      <menu>
+        <li>
+          <button>Yes</button>
+        </li>
+        <li>
+          <button>No</button>
+        </li>
+      </menu>
+    </div>
+  );
+}
+
+export default App;
